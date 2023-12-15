7242b4578be7ede48b172e2ba5f4714404279104
pc <pc@DESKTOP-A5R01JF>
2023-12-15 21:01:40 +0530

    First Commit. 11-12-2023.

---
+++ Projectjava/src/Projects/printdata.java
@@ -0,0 +1,13 @@
+package Projects;
+
+public class printdata {
+
+	public static void main(String[] args) {
+		int x=26;
+		int y=25;
+		System.out.println( x+"Twenty Six");
+		System.out.println(y+"Twenty Five");
+		
+	}
+
+}
